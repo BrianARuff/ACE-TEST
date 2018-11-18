638f4c2d2571cd3ca67f9c25c00b86c766f1b059
BrianARuff <thelonewolf198927@gmail.com>
2018-11-17 23:57:54 -0500

    prod ready

---
+++ build/precache-manifest.027c8ab956b9d2a13ecd4babf6460e8a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4c6a33372ee32f297e3d",
+    "url": "/static/js/main.4c6a3337.chunk.js"
+  },
+  {
+    "revision": "20c37c22feee374e433e",
+    "url": "/static/js/1.20c37c22.chunk.js"
+  },
+  {
+    "revision": "4c6a33372ee32f297e3d",
+    "url": "/static/css/main.d8a3b49a.chunk.css"
+  },
+  {
+    "revision": "0a5ef2c5fc2f6eb2ecc06739726e439e",
+    "url": "/index.html"
+  }
+];
